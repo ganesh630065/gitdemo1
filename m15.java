e8d8d7abea71a9e21fa77b25ca28bd07a7d2c56c
ganesh630065 <ravulaganeshece@gmail.com>
2025-03-16 18:51:38 +0530

    Add files via upload

---
+++ m15.java
@@ -0,0 +1,34 @@
+class M15
+ {
+  public static void main(String[] args)
+  {
+  /*  int num=67842901,rem=0,min=num%10,temp=num;
+    while(num!=0)
+    {                    //7839!=0  783!=0 78!=0  7!=0  
+     rem=num%10;        //7839%10=9 783%10=3  78%10=8  7%10=7
+     if(rem<min)       //0<9   3<9t  8<3f  7<3f
+       min=rem;        //9=9 3 
+     num=num/10;       //783 78 7 0
+    }
+    System.out.println(min);*/
+
+//print max number as input and excute 
+    
+
+    /*int num=98676,rem=0,rev=0,max=0,temp=num;
+    while(num!=0)
+    {
+     rem=num%10;
+     if(rem>max)
+     max=rem;
+    num/=10;
+    }
+        System.out.println("max of"+max+" is "+temp);*/
+
+
+//extraction doing in do-while loop
+
+     
+  }
+}
+     
