8f8486bdfd92f980c5a254947b16e94dc7d38745
ganesh630065 <ravulaganeshece@gmail.com>
2025-04-08 21:15:18 +0530

    Add files via upload

---
+++ methodsignature.java
@@ -0,0 +1,37 @@
+//method signature 
+class Void
+   {
+       void is1()
+       {
+         System.out.println();
+       }
+       void is1(int x)
+       {
+         System.out.println("int "+x);
+       }
+       void is1(short y)
+       {
+         System.out.println("short "+y);
+       }
+      void s1(byte b)
+       {
+          System.out.println("byte "+b);
+       }
+      void s1(long l)
+       {
+         System.out.println("long "+l);
+       }
+      void s1(double d)
+       {
+         System.out.println("double "+d);
+       }
+      void s1(float f)
+       {
+         System.out.println("float "+f);
+       }
+      public static void main(String args[])
+      {
+        Void a1=new Void();
+        a1.is1((short)10);
+      }
+}
