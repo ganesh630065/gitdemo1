3049ad2f99cdf0ce527acb1251aa9c3e7ec218a8
ganesh630065 <ravulaganeshece@gmail.com>
2025-04-20 17:37:28 +0530

    Add files via upload

---
+++ m7.java
@@ -0,0 +1,42 @@
+//perfect square root or not//
+class M7
+ {
+  public static void main(String args[])
+   {
+     /* int num1=7;
+      int res=(int)(Math.sqrt(num1));
+      if(num1/res==res)
+       {
+          System.out.println(num1 +" perfect square "+ res);
+       }
+      else
+       {
+          System.out.println(num1 +" not a perfect square "+ res);
+       }*/
+         /* int n=14;
+         double res=Math.sqrt(n);
+         if(n/res==res)
+         {
+            System.out.println(n +" perfect square "+ res);
+         }
+         else
+          {
+            System.out.println(n +" not a perfect square "+ res);
+           }*/
+//ternary operator//
+            /* int n=17;
+            double res=Math.sqrt(n);
+            System.out.println(n/res==res? n+" perfect square ": n+" not a perfect square ");*/
+              /*int n=88;
+              double res=Math.sqrt(n);
+              int t=(int)(res);
+                if((t*t)==n)
+                  {
+                    System.out.println(n+" is pertfect square ");
+                  }
+                 else
+                  {
+                     System.out.println(n+" is not a perfect square ");
+                  }*/
+    }
+ }
