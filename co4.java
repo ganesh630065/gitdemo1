685cbde74f23b0b9debbdaf958b295a5b9fa519f
ganesh630065 <ravulaganeshece@gmail.com>
2025-04-29 19:10:57 +0530

    Add files via upload

---
+++ co4.java
@@ -0,0 +1,320 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+class Co4
+{
+	public static void main(String args[])
+	{
+		/*int i=10;
+		short s=3;
+		long l=89l;
+		float f=667.8f;
+		double d=78.9;
+		ArrayList <Number> al=new ArrayList<>();
+		al.add(i);
+		al.add(s);
+		al.add(l);
+		al.add(f);
+		al.add(d);
+		System.out.println(al);*/
+
+		/*char c='p';
+		boolean b=true;
+		int i=10;
+		short s=3;
+		long l=89l;
+		float f=667.8f;
+		double d=78.9;
+		ArrayList <Object> a=new ArrayList<>();
+		a.add(i);
+		a.add(s);
+		a.add(l);
+		a.add(f);
+		a.add(d);
+		a.add(b);
+		a.add(c);
+		System.out.println(a);*/
+
+		/*int i=10;
+		ArrayList <Integer> a=new ArrayList<>();
+		a.add(i);
+		System.out.println(a);*/
+
+
+		/*char c='p';
+		ArrayList <Character> a3=new ArrayList<>();
+		a3.add(c);
+		System.out.println(a3);
+
+		boolean b=false;
+		ArrayList <Boolean> ab=new ArrayList<>();
+		ab.add(b);
+		System.out.println(ab);
+
+
+		ArrayList <String> a4=new ArrayList<>();
+		a4.add("ram");
+		a4.add("ganesh");
+		System.out.println(a4);
+
+
+		ArrayList a5=new ArrayList();
+		a5.add("ram");
+		a5.add("lava");
+		a5.add("kusa");
+		a5.add("ram");
+		System.out.println(a5);
+
+
+		ArrayList a6=new ArrayList();
+		a6.add(null);
+		System.out.println(a6);*/
+
+		//System.out.println(i+" "+System.identityHashCode(i));
+		//i=i+2;
+		//System.out.println(i+" "+System.identityHashCode(i));
+
+
+		/*LinkedList <Integer> l=new LinkedList<>();
+		l.add(12);
+		l.add(122);
+		System.out.println(l);*/
+
+		/*LinkedList l=new LinkedList();
+		l.add(12);
+		l.add(12.2);
+		l.add("pop");
+		System.out.println(l);*/
+
+		/*LinkedList l1=new LinkedList();
+		l1.add("ram");
+		l1.add("lava");
+		l1.add("kusa");
+		l1.add("ram");
+		l1.add(2,"govida");
+		System.out.println(l1);*/
+
+		/*LinkedList l2=new LinkedList();
+		l2.add(null);
+		System.out.println(l2);*/
+
+
+
+
+
+
+
+		/*Stack <Integer> s=new Stack<>();
+		s.add(12);
+		s.add(122);
+		System.out.println(s);*/
+
+		/*Stack s1=new Stack();
+		s1.add(12);
+		s1.add(12.2);
+		s1.add("pop");
+		System.out.println(s1);*/
+
+		/*Stack s2=new Stack();
+		s2.add("ram");
+		s2.add("lava");
+		s2.add("kusa");
+		s2.add("ram");
+		System.out.println(s2);*/
+
+		/*Stack s3=new Stack();
+		s3.add(null);
+		System.out.println(s3);*/
+
+
+
+
+
+
+
+		/*Vector <Integer> v=new Vector<>();
+		v.add(12);
+		v.add(122);
+		System.out.println(v);*/
+
+		/*Vector v1=new Vector();
+		v1.add(12);
+		v1.add(12.2);
+		v1.add("pop");
+		System.out.println(v1);*/
+
+		/*Vector v2=new Vector();
+		v2.add("ram");
+		v2.add("lava");
+		v2.add("kusa");
+		v2.add("ram");
+		System.out.println(v2);*/
+
+		/*Vector v3=new Vector();
+		v3.add(null);
+		v3.add(null);
+		System.out.println(v3);*/
+
+
+
+
+
+
+		/*HashSet <Integer> h=new HashSet<>();
+		h.add(5456);
+		h.add(12);
+		h.add(122);
+		System.out.println(h);*/
+
+		/*HashSet h1=new HashSet();
+		h1.add(12);
+		h1.add(12.2);
+		h1.add("pop");
+		System.out.println(h1);*/
+
+		/*HashSet h2=new HashSet();
+		h2.add("ram");
+		h2.add("lava");
+		h2.add("kusa");
+		h2.add("ram");
+		System.out.println(h2);*/
+
+		/*HashSet h3=new HashSet();
+		h3.add(null);
+		h3.add(null);
+		System.out.println(h3);*/
+
+
+
+
+		/*LinkedHashSet <Integer> l=new LinkedHashSet<>();
+		l.add(6);
+		l.add(5456);
+		l.add(12);
+		l.add(122);
+		System.out.println(l);*/
+
+		/*LinkedHashSet h1=new LinkedHashSet();
+		h1.add(12);
+		h1.add(12.2);
+		h1.add("pop");
+		System.out.println(h1);*/
+
+		/*LinkedHashSet h2=new LinkedHashSet();
+		h2.add("ram");
+		h2.add("lava");
+		h2.add("kusa");
+		h2.add("ram");
+		System.out.println(h2);*/
+
+		/*LinkedHashSet h3=new LinkedHashSet();
+		h3.add(null);
+		h3.add(null);
+		System.out.println(h3);*/
+
+
+
+		/*TreeSet <Integer> l=new TreeSet<>();
+		l.add(6);
+		l.add(5456);
+		l.add(12);
+		l.add(122);
+		System.out.println(l);*/
+
+		/*TreeSet h1=new TreeSet();
+		h1.add(12);
+		h1.add(12.2);
+		h1.add("pop");
+		System.out.println(h1);*/
+
+		/*TreeSet h2=new TreeSet();
+		h2.add("ram");
+		h2.add("lava");
+		h2.add("kusa");
+		h2.add("ram");
+		System.out.println(h2);*/
+
+		/*TreeSet h3=new TreeSet();
+		h3.add(null);
+		h3.add(null);
+		System.out.println(h3);*/
+
+
+
+		/*PriorityQueue <Integer> l=new PriorityQueue<>();
+		l.add(6);
+		l.add(5456);
+		l.add(12);
+		l.add(122);
+		System.out.println(l);*/
+
+		/*PriorityQueue h1=new PriorityQueue();
+		h1.add(12);
+		h1.add(12.2);
+		h1.add("pop");
+		System.out.println(h1);*/
+
+		/*PriorityQueue h2=new PriorityQueue();
+		h2.add("ram");
+		h2.add("lava");
+		h2.add("kusa");
+		h2.add("ram");
+		System.out.println(h2);*/
+
+		/*PriorityQueue h3=new PriorityQueue();
+		h3.add(null);
+		h3.add(null);
+		System.out.println(h3);*/
+
+
+		/*PriorityQueue h1=new PriorityQueue();
+		h1.add(12);
+		h1.add(1.23);
+		h1.add("ram");
+		h1.add('u');
+		System.out.println(h1);*/
+
+
+
+		/*ArrayDeque <Integer> l=new ArrayDeque<>();
+		l.add(6);
+		l.add(5456);
+		l.add(12);
+		l.add(122);
+		System.out.println(l);*/
+
+		/*ArrayDeque h1=new ArrayDeque();
+		h1.add(12);
+		h1.add(12.2);
+		h1.add("pop");
+		System.out.println(h1);*/
+
+		/*ArrayDeque h2=new ArrayDeque();
+		h2.add("ram");
+		h2.add("lava");
+		h2.add("kusa");
+		h2.add("ram");
+		System.out.println(h2);*/
+
+		/*ArrayDeque h3=new ArrayDeque();
+		h3.add(null);
+		h3.add(null);
+		System.out.println(h3);*/
+
+
+		/*ArrayDeque h1=new ArrayDeque();
+		h1.add(12);
+		h1.add(1.23);
+		h1.add("ram");
+		h1.add('u');
+		System.out.println(h1);*/
+
+		
+	}
+}
